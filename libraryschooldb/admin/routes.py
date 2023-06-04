d7eec7c17ef953e3ec4b06cfe23055badbcffc6b
Tilemachos Notsikas <130977352+TilemachosNots21@users.noreply.github.com>
2023-06-04 19:09:19 +0300

    Add files via upload

--- libraryschooldb/admin/routes.py
+++ libraryschooldb/admin/routes.py
@@ -203,3 +203,150 @@ def approve_operator_registrations():
     return render_template('approve_operator.html', operator_requests=operator_requests)
 
 
+@admin.route('/book_category_details', methods=['GET', 'POST'])
+def book_category_details():
+    # Fetch the list of book categories from the database
+    cursor = db.connection.cursor(DictCursor)
+    cursor.execute("SELECT CategoryName FROM Category")
+    categories = [category['CategoryName'] for category in cursor.fetchall()]
+    authors = []
+    teachers = []
+
+    if request.method == 'POST':
+        category_name = request.form['category_name']  # Get the selected book category from the form data
+        # Perform the SQL query
+        query = """
+        SELECT a.FullName AS AuthorName, CONCAT(u.FirstName, ' ', u.LastName) AS TeacherName
+        FROM Category c
+        JOIN BookCategory bc ON c.CategoryID = bc.CategoryID
+        JOIN Book b ON bc.BookID = b.BookID
+        JOIN BookAuthor ba ON b.BookID = ba.BookID
+        JOIN Author a ON ba.AuthorID = a.AuthorID
+        JOIN Borrow br ON b.BookID = br.BookID
+        JOIN SchoolUser su ON br.SchoolUserID = su.SchoolUserID
+        JOIN AppUser u ON su.SchoolUserID = u.UserID
+        WHERE c.CategoryName = %s AND su.Position = 'Teacher' AND br.Borrow_Date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+        """
+        cursor.execute(query, (category_name,))
+        results = cursor.fetchall()
+
+        print("SQL Query Results:", results)  # Print out the results of the SQL query
+
+        authors = list(set([result['AuthorName'] for result in results]))
+        teachers = list(set([result['TeacherName'] for result in results]))
+
+    cursor.close()
+
+    # Render the template with the query results and categories
+    return render_template('admin_book_category_details.html', categories=categories, authors=authors, teachers=teachers)
+
+
+@admin.route('/login/<role>/<username>/young_teachers_books', methods=['GET','POST'])
+def young_teachers_books(role, username):
+    cursor = db.connection.cursor(DictCursor)
+    query = """
+    SELECT CONCAT(u.FirstName, ' ', u.LastName) AS TeacherName, COUNT(DISTINCT b.BookID) AS NumberOfBooks
+    FROM SchoolUser su
+    JOIN AppUser u ON su.SchoolUserID = u.UserID
+    JOIN Borrow bo ON su.SchoolUserID = bo.SchoolUserID
+    JOIN Book b ON bo.BookID = b.BookID
+    WHERE su.Position = 'Teacher' AND YEAR(CURDATE()) - YEAR(u.BirthDate) < 40
+    GROUP BY su.SchoolUserID
+    ORDER BY NumberOfBooks DESC;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    return render_template('admin_young_teachers_books.html', results=results,role=role,username=username)
+
+
+@admin.route('/login/<role>/<username>/unborrowed_authors', methods=['GET'])
+def unborrowed_authors(role,username):
+    cursor = db.connection.cursor(DictCursor)
+    query = """
+    SELECT AuthorName FROM UnborrowedAuthors;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    authors = [result['AuthorName'] for result in results]
+
+    cursor.close()
+
+    return render_template('admin_unborrowed_authors.html', authors=authors, role=role, username=username)
+
+
+@admin.route('/login/<role>/<username>/operators_loan_count', methods=['GET', 'POST'])
+def operators_loan_count(role, username):
+    cursor = db.connection.cursor(DictCursor)
+    results = []
+
+    if request.method == 'POST':
+        selected_year = request.form['year']  # Get the selected year from the form data
+
+        query = """
+        SELECT o.OperatorID, CONCAT(u.FirstName, ' ', u.LastName) AS OperatorName, COUNT(b.BorrowID) AS NumberOfLoans
+        FROM Operator o
+        JOIN AppUser u ON o.OperatorID = u.UserID
+        JOIN Borrow b ON o.OperatorID = b.OperatorID
+        WHERE YEAR(b.Borrow_Date) = %s
+        GROUP BY o.OperatorID
+        HAVING NumberOfLoans > 20;
+        """
+        cursor.execute(query, (selected_year,))
+        results = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('admin_operators_loan_count.html', results=results, role=role,username=username)
+
+
+@admin.route('/login/<role>/<username>/top_category_pairs', methods=['GET'])
+def top_category_pairs(role,username):
+    cursor = db.connection.cursor(DictCursor)
+
+    query = """
+    SELECT bc1.CategoryID AS Category1, bc2.CategoryID AS Category2, COUNT(*) AS Frequency
+    FROM BookCategory bc1
+    JOIN BookCategory bc2 ON bc1.BookID = bc2.BookID AND bc1.CategoryID < bc2.CategoryID
+    JOIN Borrow b ON bc1.BookID = b.BookID
+    GROUP BY bc1.CategoryID, bc2.CategoryID
+    ORDER BY Frequency DESC
+    LIMIT 3;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('admin_top_category_pairs.html', results=results, role=role, username=username)
+
+
+@admin.route('/login/<role>/<username>/authors_with_five_less_books', methods=['GET'])
+def authors_with_five_less_books(role,username):
+    cursor = db.connection.cursor(DictCursor)
+
+    query = """
+    WITH AuthorCounts AS (
+      SELECT ba.AuthorID, a.FullName, COUNT(*) AS BookCount
+      FROM BookAuthor ba
+      JOIN Author a ON ba.AuthorID = a.AuthorID
+      GROUP BY ba.AuthorID, A.FullName
+    ),
+    MaxAuthor AS (
+      SELECT MAX(BookCount) AS MaxBooks
+      FROM AuthorCounts
+    )
+    SELECT ac.AuthorID, ac.FullName, ac.BookCount
+    FROM AuthorCounts ac, MaxAuthor ma
+    WHERE ac.BookCount >= ma.MaxBooks - 5;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('admin_authors_with_five_less_books.html', results=results, role=role, username=username)
+
+
+
